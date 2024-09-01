ddbd0a0bc080df2ec873309ee07abf3623d9fd90
Mészáros Róbert <mrobert3456@gmail.com>
2024-09-01 15:40:50 +0200

    blog onClick

--- src/_components/ui/BlogItemCard.tsx
+++ src/_components/ui/BlogItemCard.tsx
@@ -16,8 +16,14 @@ interface BlogItemCardProps {
   blog: Blog;
 }
 const BlogItemCard: React.FC<BlogItemCardProps> = ({ blog }) => {
+  const handleClick = () => {
+    window.open(blog.link, "_blank");
+  };
   return (
-    <Card className={`!rounded-none max-w-[15rem] ${hoverUnderlined}`}>
+    <Card
+      className={`!rounded-none max-w-[15rem] ${hoverUnderlined} cursor-pointer `}
+      onClick={handleClick}
+    >
       <CardBody>
         <Stack>
           <Heading size="sm">{blog.title}</Heading>
@@ -26,11 +32,19 @@ const BlogItemCard: React.FC<BlogItemCardProps> = ({ blog }) => {
       </CardBody>
       <CardFooter>
         <Flex className="!justify-between w-full">
-          <Flex className="items-center gap-2">
-            <Icon as={Image} src={blog.icon} />
+          <Flex className="items-center gap-2 ">
+            <Icon
+              as={Image}
+              src={blog.icon}
+              className="transition-all duration-200 ease-in hover:scale-150"
+            />
             <Text className="text-xs">{blog.publisher}</Text>
           </Flex>
-          <Link href={blog.link} target="_blank">
+          <Link
+            href={blog.link}
+            target="_blank"
+            className="transition-all duration-200 ease-in hover:scale-150"
+          >
             <RxExternalLink size={16} />
           </Link>
         </Flex>
