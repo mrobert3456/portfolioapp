bfd9fafcbcfa238ee3a55cb741ba4d0f44e55d72
Mészáros Róbert <mrobert3456@gmail.com>
2024-09-07 17:19:47 +0200

    blogs responsiveness fix

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -4,7 +4,7 @@ import BlogItemCard from "../_components/ui/BlogItemCard";
 const Blogs: React.FC = () => {
   return (
     <SimpleGrid id="Blogs" className="h-full">
-      <Flex className="justify-center items-center gap-2">
+      <Flex className="justify-center items-center gap-2 flex-wrap">
         {blogs.map((blog: Blog) => (
           <BlogItemCard key={`featured_blog__${blog.title}`} blog={blog} />
         ))}
