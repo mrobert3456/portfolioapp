ba0d2dbe864738790b1d0c6c200535b21ad4555f
Mészáros Róbert <mrobert3456@gmail.com>
2024-09-15 13:01:02 +0200

    customCardContainer upd

--- src/_components/ui/CustomCardContainer.tsx
+++ src/_components/ui/CustomCardContainer.tsx
@@ -5,7 +5,9 @@ const CustomCardContainer: React.FC<FlexProps> = ({
   className = "",
 }) => {
   return (
-    <div className={`${className} justify-center items-center relative p-5 `}>
+    <div
+      className={`${className} flex flex-wrap gap-2 justify-center items-center relative p-5 `}
+    >
       <div className="absolute top-0 right-0 w-[6rem] border-t-4 border-blue-500" />
       <div className="absolute top-0 right-0 h-[6rem]  border-r-4 border-blue-500" />
       <div className="absolute bottom-0 left-0 w-[6rem]  border-b-4 border-blue-500" />
