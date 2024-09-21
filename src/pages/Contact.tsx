0d9d410368243aaa3689b46039ef5a7ce4506111
Mészáros Róbert <mrobert3456@gmail.com>
2024-09-21 15:22:51 +0200

    contact form max width fix

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -6,7 +6,7 @@ const Contact: React.FC = () => {
   return (
     <SimpleGrid id="Contact" className="h-full">
       <Flex className="justify-center items-center">
-        <CustomCardContainer className="!w-1/4 !min-w-[20rem]">
+        <CustomCardContainer className="!w-1/4 !min-w-[20rem] !max-w-[20rem]">
           <ContactForm />
         </CustomCardContainer>
       </Flex>
