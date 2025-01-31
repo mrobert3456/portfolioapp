cee7acd31908dc9d43fbe67384fa358ab7b143f6
Mészáros Róbert <mrobert3456@gmail.com>
2025-01-31 19:24:58 +0100

    AgentDetails.tsx: features list

--- src/_components/rag_tool/AgentDetails.tsx
+++ src/_components/rag_tool/AgentDetails.tsx
@@ -15,35 +15,54 @@ const AgentDetails: React.FC<AgentDetails> = ({ isOpen, setIsOpen }) => {
       isOpen={isOpen}
       onClose={() => setIsOpen(false)}
     >
-      {Object.entries(AGENT).map(([key, value]: [string, string | Content]) => (
-        <SimpleGrid
-          key={`field__${key}`}
-          columns={2}
-          gridTemplateColumns={"0.6fr 1fr"}
+      <div style={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
+        <div
+          style={{ display: "flex", flexDirection: "column", gap: "0.5rem" }}
         >
-          <Text className="capitalize text-xs">{key}</Text>
-          {typeof value !== "string" ? (
-            <div className="flex gap-2 items-center">
-              {<Icon as={value.icon} color={"blue.600"} />}
-              {value.url ? (
-                <Link
-                  className="text-xs"
-                  key={`${value.url}`}
-                  aria-label={value.name}
-                  href={value.url}
-                  target="_blank"
-                >
-                  {value.name}
-                </Link>
-              ) : (
-                <Text className="text-xs">{value.name}</Text>
-              )}
-            </div>
-          ) : (
-            <Text className="capitalize text-xs">{value}</Text>
+          {Object.entries(AGENT).map(
+            ([key, value]: [string, string | Content]) => (
+              <SimpleGrid
+                key={`field__${key}`}
+                columns={2}
+                gridTemplateColumns={"0.6fr 1fr"}
+              >
+                <Text className="capitalize text-xs">{key}</Text>
+                {typeof value !== "string" ? (
+                  <div className="flex gap-2 items-center">
+                    {<Icon as={value.icon} color={"blue.600"} />}
+                    {value.url ? (
+                      <Link
+                        className="text-xs"
+                        key={`${value.url}`}
+                        aria-label={value.name}
+                        href={value.url}
+                        target="_blank"
+                      >
+                        {value.name}
+                      </Link>
+                    ) : (
+                      <Text className="text-xs">{value.name}</Text>
+                    )}
+                  </div>
+                ) : (
+                  <Text className="capitalize text-xs">{value}</Text>
+                )}
+              </SimpleGrid>
+            )
           )}
-        </SimpleGrid>
-      ))}
+        </div>
+        <div className="flex-col">
+          <h3 className="text-sky-500">Features coming soon</h3>
+          <ul>
+            <li key="option-1" className="text-sm text-gray-500">
+              Conversational memory
+            </li>
+            <li key="option-2" className="text-sm text-gray-500">
+              Hallucination grading
+            </li>
+          </ul>
+        </div>
+      </div>
     </CustomDrawer>
   );
 };
