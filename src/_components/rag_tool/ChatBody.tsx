e6b46e199e38adf850cd59d2d4417589df2c28e5
Mészáros Róbert <mrobert3456@gmail.com>
2025-03-22 19:44:25 +0100

    Chat desing upd

--- src/_components/rag_tool/ChatBody.tsx
+++ src/_components/rag_tool/ChatBody.tsx
@@ -1,4 +1,12 @@
-import { Tag, Icon, Progress, useColorModeValue } from "@chakra-ui/react";
+import {
+  Icon,
+  Text,
+  useColorModeValue,
+  Box,
+  Flex,
+  Spinner,
+} from "@chakra-ui/react";
+import { useRef, useEffect } from "react";
 import { CiUser } from "react-icons/ci";
 
 interface ChatBodyProps {
@@ -8,37 +16,51 @@ interface ChatBodyProps {
 }
 const ChatBody: React.FC<ChatBodyProps> = ({ messages, isLoading, error }) => {
   const profileColors = useColorModeValue("!bg-slate-300", "!bg-slate-700");
+  const userMessageBg = useColorModeValue("!bg-slate-300", "!bg-slate-800");
+
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
 
   return (
-    <div className="flex-1">
-      {messages.map((value: string, index: number) => {
-        if (index % 2 === 0) {
-          return (
-            <p className="text-right">
-              <Tag className="p-2">{value}</Tag>
-            </p>
-          );
-        }
-        return (
-          <div className="flex gap-2 items-center">
-            <Icon
-              w={30}
-              h={30}
-              p={1}
-              className={`rounded-full ${profileColors}`}
+    <Box className="flex-1 gap-3 p-4 overflow-y-auto">
+      <Flex direction="column" gap={3} align="flex-start">
+        {messages.map((message, index) => (
+          <Flex
+            key={index}
+            align="center"
+            gap={2}
+            justify={index % 2 === 0 ? "flex-end" : "flex-start"}
+            alignSelf={index % 2 === 0 ? "flex-end" : "flex-start"}
+          >
+            {index % 2 !== 0 && (
+              <Icon
+                w={30}
+                h={30}
+                p={1}
+                className={`rounded-full ${profileColors}`}
+              >
+                <CiUser size={24} />
+              </Icon>
+            )}
+            <Box
+              className={`p-2 rounded-lg break-words ${
+                index % 2 === 0 && userMessageBg
+              }`}
             >
-              <CiUser size={24} />
-            </Icon>
-            {value}
-          </div>
-        );
-      })}
-
-      {isLoading && !error && (
-        <Progress isIndeterminate className="w-10 rounded" />
-      )}
-      {error && !isLoading && <p>{error}</p>}
-    </div>
+              <Text>{message}</Text>
+            </Box>
+          </Flex>
+        ))}
+        <div ref={messagesEndRef} />
+      </Flex>
+      {isLoading && !error && <Spinner size="sm" color="blue.500" />}
+      {error && !isLoading && <Text color="red.500">{error}</Text>}
+    </Box>
   );
 };
 
